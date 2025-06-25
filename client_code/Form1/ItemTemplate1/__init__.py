4a359e368dd2de7f7e8257311a6a52be2de0cff0
os.smerfetka@gmail.com <os.smerfetka@gmail.com>
2025-06-25 19:22:42 +0000

    Created Form1.RowTemplate2, Form1.ItemTemplate1 and Form1.RowTemplate1; Edited settings and Form1

--- client_code/Form1/ItemTemplate1/__init__.py
+++ client_code/Form1/ItemTemplate1/__init__.py
@@ -1,15 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 
-
-class Form1(Form1Template):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    print('elo')
-    pass
--- client_code/Form1/RowTemplate2/__init__.py
+++ client_code/Form1/RowTemplate2/__init__.py
@@ -1,15 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import RowTemplate2Template
 from anvil import *
 
-
-class Form1(Form1Template):
+class RowTemplate2(RowTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    print('elo')
-    pass
