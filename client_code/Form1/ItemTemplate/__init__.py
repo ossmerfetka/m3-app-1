618d18231880053ebc6cd3e84e53451a200d8b30
os.smerfetka@gmail.com <os.smerfetka@gmail.com>
2025-06-26 19:08:56 +0000

    Created Form1.ItemTemplate1; Edited Form1.ItemTemplate, Form1.RowTemplate1, Form1 and Form1.ItemTemplate2; Deleted Form1.ItemTemplate3

--- client_code/Form1/ItemTemplate/__init__.py
+++ client_code/Form1/ItemTemplate/__init__.py
@@ -10,4 +10,4 @@ class ItemTemplate(ItemTemplateTemplate):
   def form_show(self, **event_args):
     self.label_name.text = self.item['name']
     self.label_price.text = self.item['description']
-    self.image.source = self.item['image_path']
+    self.image.source = f"http://localhost:8000/{self.item['image_path']}"
